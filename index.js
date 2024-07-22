f398a6fd307dfdb89245bcd7c5ee7a28bc867cbd
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-07-22 19:03:55 +0300

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+console.dir(document);
+
+// const h1 = document.getElementById('main-title');
+// console.dir(h1);
+// h1.style.textAlign = 'center';
+// h1.style.color = 'green'
+// h1.style.backgroundColor ='yellow'
+
+// h1.textContent = 'dfdfgdfg'
+// h1.className ='heading';
+
+// const p =document.getElementById('text')
+// console.dir(p);
+// p.style.fontSize = '24px';
+// p.textContent = 'dbdgdfg';
+
+
+// const word = 'image'
+// const img =document.getElementById(word)
+// console.dir(img);
+// console.log(img);
+// img.src = 'https://24tv.ua/resources/photos/news/202208/2120650.jpg?v=1660120254000' ;
+
+// img.alt = 'turtle';
+
+
+const button =document.getElementById('btn');
+console.dir(btn);
+btn.style.width = '200px';
+btn.style.height = '700px';
+
+
