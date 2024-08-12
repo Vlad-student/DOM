c0938efa4941be3939057a7f3112bd1c8975f056
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-08-12 19:06:17 +0300

    рпро

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
+// const button = document.getElementById('uniq');
+//     button.style.backgroundColor = 'green';
 
-// const cont =document.getElementById('content');
-// console.dir(cont);
-// content.className = 'content';
-
-
-// const btn =document.getElementById('btn');
-// console.dir(btn);
-
-// btn.onclick =function handleClick () {
-//     alert (' you make click on button')
-// }
-// btn.onclick =function () {
-//     content.textContent ='qwerty';
-// }
-
-// btn.addEventListener('click' , function () {
-//     btn.style.width = '200px';
-// })
-
-// btn.addEventListener('click',function() {
-//     btn.textContent = btn.textContent ==='off' ?'on': 'off';
-//    content.textContent =  btn.textContent === 'off' ? 'close' : 'open';
+    //  const buttons = document.getElementsByClassName('btn');
+    //  for (const btn of buttons) {
+    //     btn.style.display = 'block' ;
+    //  }
+    //     console.dir(buttons);
    
-// })
 
+         const buttons = document.getElementsByClassName('btn');
+ for (const btn of buttons) {
+btn.addEventListener('click', ()=>{
+btn.style.fontSize='30px'  
+    })
+ }
+
+const h2Block = document.querySelector('#block> h2')
+console.dir(h2Block);
+h2Block.style.color ='green'
+
+ const btnsBlock = document.querySelectorAll('#block>.btn')
+ console.dir(btnsBlock);
+
+ btnsBlock.forEach(element => {
+    element.style.backgroundColor ='yellow'
+ });
+
+
+const allLi = document.querySelectorAll('#block>ol>li')
+allLi.forEach(element => {
+    element.addEventListener('click', ()=>{
+        element.style.textContent = 'WoW' 
+})
+})
+;
+
+const item1 = document.querySelector('#block>ol>li')
+item1.forEach(item1 => {
+    item1.addEventListener( 'click', ()=>{
+item1.style.backgroundColor = 'pink';
+    })
+});
 
 
