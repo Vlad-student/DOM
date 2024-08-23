13ed1ee7a39a8fded097ae802ae88fda87ac60e3
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-08-23 17:41:13 +0300

    btns

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const root = document.getElementById('root');
-console.dir(root)
+const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"];
 
-const h1 = document.createElement('h1');
-h1.textContent = ' i create h1';
-h1.classList.add('heading');
-h1.addEventListener('click', ()=>{
-    h1.style.color = 'red'
+const btns = colors.map((color)=>{
+    const btn = document.createElement('button');
+    btn.textContent = color;
+    btn.style.backgroundColor = color;
+return btn;
 })
-console.dir(h1);
-console.log(h1);
 
-const btn = document.createElement('button');
-btn.textContent = 'click me'
+root.append(...btns)
 
-const em = document.createElement('em');
-em.textContent ='!!!!'
 
-h1.append(em);
 
-root.append(btn, h1)
+
+
+// console.dir(root)
+
+// const h1 = document.createElement('h1');
+// h1.textContent = ' i create h1';
+// h1.classList.add('heading');
+// h1.addEventListener('click', ()=>{
+//     h1.style.color = 'red'
+// })
+// console.dir(h1);
+// console.log(h1);
+
+// const btn = document.createElement('button');
+// btn.textContent = 'click me'
+
+// const em = document.createElement('em');
+// em.textContent ='!!!!'
+
+// h1.append(em);
+
+// root.append(btn, h1)
+
+
+
+
